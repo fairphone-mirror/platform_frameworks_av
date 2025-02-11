7da4469f6a622185279e82b604e4a26902f8ade1
juting.huang <juting.huang@tcl.com>
2025-02-11 20:01:02 +0800

    [FPS-620] FPS cameraalgoservice porting
     ###%%%comment: FPS cameraalgoservice porting
     ###%%%bug number:FPS-620
     ###%%%product name:FP Spring
     ###%%%Root Cause: FPS cameraalgoservice porting
     ###%%%Root Cause Detail: FPS cameraalgoservice porting
     ###%%%Test_Suggestion:test camera
     ###%%%Test_Report:PASS
     ###%%%Bug category:Platform
     ###%%%Module_Impact:NO
     ###%%%Solution:NO
     ###%%%Pre_build:YES
     ###%%%CodeReviewModule:Camera-system
     ###%%%Highlight:NO
     ###%%%author email:juting.huang
    
    Change-Id: I19548c3186aeccbd7df0d2744e923408f2691297

--- services/camera/libcameraservice/fputils/FPCameraHelper.cpp
+++ services/camera/libcameraservice/fputils/FPCameraHelper.cpp
@@ -15,7 +15,7 @@
 #include <libxml/tree.h>
 #include <libxml/parser.h>
 
-#define DUMMY_FRAME 2
+#define DUMMY_FRAME 1
 
 using aidl::vendor::tcl::camera::algoservice::HandleParams;
 using aidl::vendor::tcl::camera::algoservice::ITctCameraAlgoService;
