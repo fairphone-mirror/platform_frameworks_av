cc448e6511de4f8976b8f0fbee8ba8448d0c37ad
binchang.liang <binchang.liang@tcl.com>
2025-05-15 17:35:03 +0800

    [FPS-2543]when faceunlock and double clicking power button,open system camera priority first
     ###%%%comment:when faceunlock and double clicking power button,open system camera priority first
     ###%%%bug number:FPS-2543
     ###%%%product name:FP Spring
     ###%%%Root Cause:FP Spring camera modify
     ###%%%Root Cause Detail:FP6 camera modify
     ###%%%Test_Suggestion:test camera
     ###%%%Test_Report:PASS
     ###%%%Bug category:Platform
     ###%%%Module_Impact:NO
     ###%%%Solution:NO
     ###%%%Pre_build:YES
     ###%%%CodeReviewModule:Camera-system
     ###%%%Highlight:NO
     ###%%%author:binchang.liang
    
    Change-Id: I6ddcc3dda8e18a50649f3fc22fb6f1d79f672305

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1999,6 +1999,9 @@ status_t CameraService::handleEvictionsLocked(const std::string& cameraId, int c
                 return err;
             }
 
+            //start add by binchang.liang
+            handleFPSEvictionsLocked(packageName, &mActiveClientManager, ownerPids.size(), &ownerPids[0], &priorityScores[0]);
+            //end add by binchang.liang
             // Update all active clients' priorities
             std::map<int,resource_policy::ClientPriority> pidToPriorityMap;
             for (size_t i = 0; i < ownerPids.size() - 1; i++) {
@@ -4370,6 +4373,12 @@ bool CameraService::BasicClient::isValidAudioRestriction(int32_t mode) {
 
 status_t CameraService::BasicClient::handlePermissionResult(
         PermissionChecker::PermissionResult result) {
+    //Begin add by binchang.liang
+    if (mClientPackageName == "com.fps.camera" && mode == AppOpsManager::MODE_IGNORED) {
+        mode = AppOpsManager::MODE_ALLOWED;
+        ALOGI("handleAppOpMode Adding MODE_ALLOWED for com.fps.camera");
+    }
+    //End add by binchang.liang
     if (result == PermissionChecker::PERMISSION_HARD_DENIED) {
         ALOGI("Camera %s: Access for \"%s\" has been revoked", mCameraIdStr.c_str(),
               getPackageName().c_str());
@@ -6999,5 +7008,52 @@ void CameraService::clearInjectionParameters() {
     mInjectionExternalCamId = "";
     mInjectionStatusListener->removeListener();
 }
+
+//start add by binchang.liang
+/**
+ * The lower the score, the higher the priority.
+ * Don't change content of currentActiveClientManager, only override scores.
+ * ownerPids's last value is incoming client
+ */
+ const char* CameraService::FpsCameraPriorityLowerAppList[] = {
+    "com.android.settings"
+};
+void CameraService::handleFPSEvictionsLocked(std::string incomingPackageName __attribute__((unused)), CameraService::CameraClientManager *currentActiveClientManager,
+                                                size_t ownerPidsSize, int32_t *ownerPids, int32_t *scores) {
+    for (auto& client : currentActiveClientManager->getAll()) {
+        auto clientSp = client->getValue();
+        if(clientSp != nullptr) {
+            std::string clientName =  clientSp->getPackageName();
+            int clientPid = clientSp->getClientPid();
+            //Begin modified by xiaoming-zhong for [defect][11604024][evicting conflicting client] on 2021/10/14
+            //Make app in FpsCameraPriorityLowerAppList priority lower.
+            for(size_t i = 0; i < (sizeof(FpsCameraPriorityLowerAppList)/sizeof(*FpsCameraPriorityLowerAppList)); i++) {
+                std::string tmp(FpsCameraPriorityLowerAppList[i]);
+                if(tmp == clientName) {
+                    for(size_t j = 0; j < ownerPidsSize; j++) {
+                        if(ownerPids[j] == clientSp->getClientPid()) {
+                            *(scores+j) = resource_policy::SERVICE_ADJ;
+                            ALOGI("current package: %s. pid %d,scores %d", clientName.c_str(), clientPid,resource_policy::SERVICE_ADJ);
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            //End modified by xiaoming-zhong for [defect][11604024][evicting conflicting client] on 2021/10/14
+        }
+    }
+    //Begin modified by xiaoming-zhong for [defect][11604024][evicting conflicting client] on 2021/10/14
+    //Make app in FpsCameraPriorityLowerAppList priority lower.
+    for(size_t i = 0; i < (sizeof(FpsCameraPriorityLowerAppList)/sizeof(*FpsCameraPriorityLowerAppList)); i++) {
+        std::string tmp(FpsCameraPriorityLowerAppList[i]);
+        if(tmp == incomingPackageName) {
+            *(scores+(ownerPidsSize-1)) = resource_policy::SERVICE_ADJ;
+            break;
+        }
+    }
+    //End modified by xiaoming-zhong for [defect][11604024][evicting conflicting client] on 2021/10/14
+}
+//end add by binchang.liang
 
 } // namespace android
